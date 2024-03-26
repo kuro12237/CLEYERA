10f28a507f5cb78d1d9a0e1ec88ee02446c1f3b1
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2024-03-27 02:27:55 +0900

    NormalObject3dのNOrmalを補正

--- Resources/Shader/Model/NormalObject3d.PS.hlsl
+++ Resources/Shader/Model/NormalObject3d.PS.hlsl
@@ -8,8 +8,8 @@ SamplerState gSampler : register(s0);
 PixelShaderOutput main(NormalVertexShaderOutput input)
 {
     PixelShaderOutput output;
-    
-    float4 transformedUV = float4(input.texcoord, 0.0f, 1.0f); //mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uv);
+
+    float4 transformedUV = float4(input.texcoord, 0.0f, 1.0f);
     float4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     float3 result = normalize(input.normal.rgb + textureColor.rgb);
     float3 N = normalize(textureColor.rgb+ normalize(mul(input.normal.xyz, (float32_t3x3)input.worldMatrix)));
