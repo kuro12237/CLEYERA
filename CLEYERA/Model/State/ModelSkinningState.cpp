ec6f781accee4cfc5f5273f3d08fe2d89cf24c49
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2024-05-16 17:09:38 +0900

    save

--- CLEYERA/Model/State/ModelSkinningState.cpp
+++ CLEYERA/Model/State/ModelSkinningState.cpp
@@ -14,12 +14,35 @@ void ModelSkinningState::Initialize(Model* state)
 	index_ = make_unique<BufferResource<uint32_t>>();
 	index_->CreateResource(uint32_t(state->GetModelData().indecs.size()));
 	index_->CreateIndexBufferView();
-
+	//influenceの作成
 	influence_ = make_unique<BufferResource<VertexInfluence>>();
-	influence_->CreateResource(sizeof(VertexInfluence) * state->GetModelData().vertices.size());
-	//VertexInfluence* mappedInfuence = nullptr;
+	influence_->CreateResource(uint32_t(sizeof(VertexInfluence) * state->GetModelData().vertices.size()));
 	influence_->CreateIndexBufferView();
+	
+	influence_->Map();
+	vector<VertexInfluence> mappedInfuence;
+	mappedInfuence.resize(state->GetModelData().vertices.size());
 
+	SAnimation::Skeleton skeleton = state->GetModelData().node.skeleton;
+	for (const auto& jointWeight : state->GetModelData().skinClusterData) {
+		auto it = skeleton.jointMap.find(jointWeight.first);
+		if (it == skeleton.jointMap.end()) {
+			continue;
+		}
+		for (const auto& vertexWeight : jointWeight.second.vertexWeights) {
+			auto& currentInfluence = mappedInfuence[vertexWeight.vertexIndex];
+			// 空いているところに入れる
+			for (uint32_t index = 0; index < kNumMaxInfluence; ++index) {
+				// weight == 0 が空いている状態 その場所に代入
+				if (currentInfluence.weights[index] == 0.0f) {
+					currentInfluence.weights[index] = vertexWeight.weight;
+					currentInfluence.jointIndicess[index] = (*it).second;
+					break;
+				}
+			}
+		}
+	}
+	influence_->Setbuffer(mappedInfuence);
 }
 
 void ModelSkinningState::CallPipelinexVertex(Model* state)
