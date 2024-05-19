f488f0ddf6fb51df799663642da639fe47ad9b35
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2024-05-19 20:08:28 +0900

    save

--- CLEYERA/DebugTool/DebugSkeleton/DebugSkeleton.cpp
+++ CLEYERA/DebugTool/DebugSkeleton/DebugSkeleton.cpp
@@ -2,8 +2,36 @@
 
 void DebugSkeleton::Create(SAnimation::Skeleton skeleton, WorldTransform w)
 {
+	skeleton_ = &skeleton;
+	wT_ = &w;
+
+	const size_t jointCount = skeleton_->joints.size();
+
+
+
+
 }
 
 void DebugSkeleton::Draw(CameraData camera)
 {
+
+
+}
+
+void DebugSkeleton::CreateJoint(size_t size)
+{
+	jointWt_.resize(size);
+	jointObject_.resize(size);
+
+	jointObjectDesc_.useLight = true;
+	jointModelHandle_ = ModelManager::LoadObjectFile("DebugTestBox");
+	for (size_t i = 0; i < size; i++)
+	{
+		jointWt_[i].Initialize();
+		jointObject_[i] = make_unique<Game3dObject>();
+		jointObject_[i]->Create(make_unique<Phong3dPipline>());
+		jointObject_[i]->SetDesc(jointObjectDesc_);
+		jointObject_[i]->SetModel(jointModelHandle_);
+	}
+
 }
--- CLEYERA/DebugTool/DebugSkeleton/DebugSkeleton.h
+++ CLEYERA/DebugTool/DebugSkeleton/DebugSkeleton.h
@@ -1,1 +1,30 @@
 #pragma once
+#include"GraphicsPipelineManager.h"
+#include"CreateResource.h"
+#include"CameraData.h"
+#include"ModelManager.h"
+#include"Game3dObject.h"
+
+class DebugSkeleton
+{
+public:
+	DebugSkeleton() {};
+	~DebugSkeleton() {};
+
+	void Create(SAnimation::Skeleton skeleton,WorldTransform w);
+
+	void Draw(CameraData camera);
+
+private:
+
+	void CreateJoint(size_t size);
+
+	const SAnimation::Skeleton* skeleton_;
+	const WorldTransform* wT_;
+
+	uint32_t jointModelHandle_ = 0;
+	Game3dObjectDesc jointObjectDesc_ = {};
+	vector<WorldTransform>jointWt_;
+	vector<unique_ptr<Game3dObject>>jointObject_;
+
+};
