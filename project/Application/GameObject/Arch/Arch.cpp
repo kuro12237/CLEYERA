806807ecfb62240b1d543075e433eee33e68506e
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2024-12-05 10:21:12 +0900

    橋クラス修正

--- project/Application/GameObject/Arch/Arch.cpp
+++ project/Application/GameObject/Arch/Arch.cpp
@@ -2,26 +2,10 @@
 
 void Arch::Initialize()
 {
-
 	name_ = "ArchBridge";
-
-	auto& desc = gameObjectManager_->GetObjectDesc(name_);
-
-	//edgeÝ’è
-	desc.edgeDesc.minmax = { -0.1f,0.2f };
-	desc.edgeDesc.mask = 1.0f;
-	desc.edgeDesc.edgeColor = { 0.0f,0.0f,1.0f,0.0f };
 }
 
 void Arch::Update()
 {
-	if (isStart_)
-	{
-		auto& desc = gameObjectManager_->GetObjectDesc(name_);
-		if (desc.edgeDesc.mask >= 0.0f)
-		{
-			desc.edgeDesc.mask -= 1.0f / 120.0f;
-		}
-	}
 
 }
