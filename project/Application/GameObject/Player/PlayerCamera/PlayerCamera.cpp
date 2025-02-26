0f7b86ff23956de2b9bc4c642a42dd85a0a8b561
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2025-02-26 15:36:05 +0900

    checkMedia

--- project/Application/GameObject/Player/PlayerCamera/PlayerCamera.cpp
+++ project/Application/GameObject/Player/PlayerCamera/PlayerCamera.cpp
@@ -2,42 +2,49 @@
 
 using namespace Engine::Transform;
 
-
 void PlayerCamera::Initialize()
 {
-	name_ = "PlayerCamera";
-	
-	objectData_ = gameObjectManager_->GetCameraData(name_);
-	auto transform = objectData_.lock()->GetWorldTransform().transform;
-	transform.scale = { 1.0f,1.0f,1.0f };
-	transform.rotate.x = 0.1f;
+   name_ = VAR_NAME(PlayerCamera);
+
+   objectData_ = gameObjectManager_->GetCameraData(name_);
+   auto transform = objectData_.lock()->GetWorldTransform().transform;
+   
+   this->jsonGropName_ = name_;
+   CreateJsonData();
+
+   AddJsonItem<decltype(cameraRotateX_)>(VAR_NAME(cameraRotateX_), cameraRotateX_);
+   cameraRotateX_ = GetJsonItem<decltype(cameraRotateX_)>(VAR_NAME(cameraRotateX_));
+   transform.rotate.x = cameraRotateX_;
+
+   AddJsonItem<decltype(cameraFacter_)>(VAR_NAME(cameraFacter_), cameraFacter_);
+   cameraFacter_ = GetJsonItem<decltype(cameraFacter_)>(VAR_NAME(cameraFacter_));
+
+   AddJsonItem<decltype(cameraOffset_)>(VAR_NAME(cameraOffset_), cameraOffset_);
+   cameraOffset_ = GetJsonItem<decltype(cameraOffset_)>(VAR_NAME(cameraOffset_));
 }
 
 void PlayerCamera::Update()
 {
-	auto& cameraWt = objectData_.lock()->GetWorldTransform();
+   auto &cameraWt = objectData_.lock()->GetWorldTransform();
 
-	if (target_)
-	{
-		interTarget_ = Math::Vector::Lerp(interTarget_, target_->transform.translate, 0.25f);
-	}
+   if (target_) {
+      interTarget_ = Math::Vector::Lerp(interTarget_, target_->transform.translate, cameraFacter_);
+   }
 
-	Math::Vector::Vector3 offset ={ 0.0f,0.0f,-64.0f };
-	offset = CalcOffset(cameraWt, offset);
-	cameraWt.transform.translate = Math::Vector::Add(interTarget_, offset);
+   Math::Vector::Vector3 offset = cameraOffset_;
+   offset = CalcOffset(cameraWt, offset);
+   cameraWt.transform.translate = Math::Vector::Add(interTarget_, offset);
 }
 
-void PlayerCamera::Shake()
-{
-	
-}
+void PlayerCamera::Shake() {}
 
-Math::Vector::Vector3 PlayerCamera::CalcOffset(const WorldTransform& w, const Math::Vector::Vector3 v) const
+Math::Vector::Vector3 PlayerCamera::CalcOffset(const WorldTransform &w,
+                                               const Math::Vector::Vector3 v) const
 {
-	Math::Matrix::Matrix4x4 rotateMatrix = Math::Matrix::RotateXYZMatrix(
-		w.transform.rotate.x, w.transform.rotate.y, w.transform.rotate.z);
+   Math::Matrix::Matrix4x4 rotateMatrix = Math::Matrix::RotateXYZMatrix(
+       w.transform.rotate.x, w.transform.rotate.y, w.transform.rotate.z);
 
-	Math::Vector::Vector3 offset = v;
-	offset = Math::Vector::TransformNormal(offset, rotateMatrix);
-	return offset;
+   Math::Vector::Vector3 offset = v;
+   offset = Math::Vector::TransformNormal(offset, rotateMatrix);
+   return offset;
 }
--- project/Application/GameObject/Player/PlayerCamera/PlayerCamera.h
+++ project/Application/GameObject/Player/PlayerCamera/PlayerCamera.h
@@ -1,35 +1,34 @@
 #pragma once
 
-#include"Utility/ObjectManager/GameObjectManager.h"
-#include"GameObject/ObjectInterface/ObjectComponent.h"
+#include "GameObject/ObjectInterface/ObjectComponent.h"
+#include "Utility/ObjectManager/GameObjectManager.h"
 
 /// <summary>
 /// プレイヤーカメラ
 /// </summary>
-class PlayerCamera:public ObjectComponent
+class PlayerCamera : public ObjectComponent
 {
-public:
-	PlayerCamera() {};
-	~PlayerCamera() {};
+ public:
+   PlayerCamera() {};
+   ~PlayerCamera() {};
 
-	/// <summary>
-	/// 初期化
-	/// </summary>
-	void Initialize()override;
+   /// <summary>
+   /// 初期化
+   /// </summary>
+   void Initialize() override;
 
-	/// <summary>
-	/// 更新
-	/// </summary>
-	void Update()override;
+   /// <summary>
+   /// 更新
+   /// </summary>
+   void Update() override;
 
 #pragma region Set
 
-void SetTarget(const Engine::Transform::WorldTransform &t, string name)
-        {
-           targetName_ = name;
-           target_ = &t;
-        }
-
+   void SetTarget(const Engine::Transform::WorldTransform &t, string name)
+   {
+      targetName_ = name;
+      target_ = &t;
+   }
 
 #pragma endregion
 
@@ -37,20 +36,22 @@ void SetTarget(const Engine::Transform::WorldTransform &t, string name)
 
 #pragma endregion
 
-private:
+ private:
+   void Shake();
 
-	void Shake();
+   Math::Vector::Vector3 CalcOffset(const Engine::Transform::WorldTransform &w,
+                                    const Math::Vector::Vector3 v) const;
+   string targetName_ = "";
+   const Engine::Transform::WorldTransform *target_ = nullptr;
 
-	Math::Vector::Vector3 CalcOffset(const Engine::Transform::WorldTransform& w ,const Math::Vector::Vector3 v)const;
+   Math::Vector::Vector3 interTarget_{};
+   float distructionAngleY_ = 0.0f;
 
-	string targetName_ = "";
-	const Engine::Transform::WorldTransform* target_ = nullptr;
+   Math::Vector::Vector2 cameraShakeRadious_ = {-1.0f, 1.0f};
+   bool isShake_ = false;
 
-	Math::Vector::Vector3 interTarget_{};
-	float distructionAngleY_ = 0.0f;
-	float lerp_t = 0.0f;
+   float cameraFacter_ = 0.0f;
+   Math::Vector::Vector3 cameraOffset_ = {};
 
-	Math::Vector::Vector2 cameraShakeRadious_ = { -1.0f,1.0f };
-	bool isShake_ = false;
+   float cameraRotateX_ = 0.0f;
 };
-
